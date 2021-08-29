2c34c3d27bba37390a61ee8270127ad8955d1814
Zachary Sain <zachsain@gmail.com>
2021-08-29 13:45:27 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,58 @@
+function add (num1, num2){
+    return num1 + num2;
+}
+
+console.log(add(1, 2))
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+console.log(subtract(3, 4));
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+console.log(multiply(5, 5));
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+console.log(divide(20, 4));
+
+function increment(n) {
+    n++;
+    return n;
+}
+
+function decrement(n) {
+    n--;
+    return n;
+}
+
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n, 10);
+}
+
+/*
++ = addition 
+- = subtraction 
+* = multiplication 
+/ = division 
+
+
+++ = increment 
+-- = decrement 
++= = add and set 
+-= = minus and set
+*= = multiply and set
+/= = divide and set
+
+*/ 
+
